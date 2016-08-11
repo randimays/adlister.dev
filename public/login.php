a80ff1f21602a57bebf8678a4a692eed14b1b109
Anthony <martab7.am@gmail.com>
2016-08-11 11:02:26 -0500

    Updates branch

--- public/login.php
+++ public/login.php
@@ -23,18 +23,20 @@
           <h2>REGISTERED SHOPPERS</h2>
           <p>
             Please provide your email address and password to access your account.
+            <br>
+            <span class="asterisk" id="required">*Required fields.</span>
           </p>
           <form name="LogonForm" action="Logon" id="LogonForm" method="post">
             <div class="form-item">
               <label for="logonId">
-                "Email Address" <span class="asterisk">*</span>
+                Email Address <span class="asterisk">*</span>
               </label>
               <input type="text" name="logonId" maxlength="254" title="Email Address" value="">
               <br>
             </div>
             <div class="form-item">
               <label for="logonPassword">
-                "Password" <span class="asterisk">*</span>
+                Password <span class="asterisk">*</span>
               </label>
               <input type="text" name="logonPassword" maxlength="254" title="Password" value="">
               <br>
@@ -63,31 +65,37 @@
           <form class="" action="index.html" method="post">
             <div class="form-item">
               <label for="register_email">
-                "Email Address" <span class="asterisk">*</span>
+                Email Address <span class="asterisk">*</span>
               </label>
-              <input type="text" title="Register Email Address" maxlength="50" id="regiter_email" name="email1" value="">
+              <input type="text" title="Register Email Address" maxlength="32" id="regiter_email" name="email1" value="">
             </div>
             <div class="form-item">
               <label for="register_password">
-                "Password"
+                Password
               </label>
-              <input class="passwordField" type="password" title="Register Password" maxlength="20" id="register_password" name="password1" value="">
+              <input class="passwordField" type="password" title="Register Password" maxlength="64" id="register_password" name="password1" value="">
             </div>
             <div class="form-item">
               <label for="regitster_passwordVerify">
-                "Confirm Password" <span class="asterisk">*</span>
+                Confirm Password <span class="asterisk">*</span>
               </label>
-              <input class="passwordConfirmField" type="password" title="Confirm Password" maxlength="20" id="register_passwordConfirm" name="password2"value="">
+              <input class="passwordConfirmField" type="password" title="Confirm Password" maxlength="64" id="register_passwordConfirm" name="password2"value="">
             </div>
             <div class="form-item">
-              <button type="submit" name="register" title="Register" class="submit costco-button"></button>
+              <button type="submit" name="register" title="Register" class="submit costco-button">
                 <span class="s1">
                   <span class="s2">Register</span>
                 </span>
+              </button>
             </div>
           </form>
+          <div class="disclaimer">
+            <p>
+              By registering or purchasing from Costco.com, you are agreeing to the <a href="#">terms and conditions</a> of use.
+            </p>
+          </div>
 
-          
+
         </div>
       </div>
     </div>

7c85a26c71274ebada2981968cffed1661763b98
Randi Mays <randimays1024@gmail.com>
2016-08-11 16:31:29 -0500

    Updates to reconcile column conflicts

--- database/migrations/items_migration.php
+++ database/migrations/items_migration.php
@@ -4,12 +4,12 @@ require_once __DIR__ . '/../db_connect.php';
 $query='DROP TABLE IF EXISTS  items';
 $dbc->exec($query);
 $query='CREATE TABLE IF NOT EXISTS items(
-		id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+		item_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 		item_name VARCHAR(100) NOT NULL,
 		item_price INT NOT NULL,
 		item_description TEXT,
-		img_path VARCHAR(100) NOT NuLL,
+		img_path VARCHAR(100) NOT NULL,
 		short_description VARCHAR(200),
-		PRIMARY KEY(id)
+		PRIMARY KEY(item_id)
 );';
 $dbc->exec($query);

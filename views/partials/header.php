3eb06206c4cfb54f8d3ccb2278b744a847981deb
Randi Mays <randimays1024@gmail.com>
2016-08-11 10:56:36 -0500

    Moved Brandon's navbar images into public folder. Added navbar CSS into main CSS file. Started header.php and navbar.php files with appropriate consistent code. Created DB connect file for connection to database. Added .DS_Store to gitignore file. Started piecing together homepage.

---
+++ views/partials/header.php
@@ -0,0 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,500,700,300,400italic,500italic,300italic,700italic" rel="stylesheet" type="text/css">
+<link href="/css/main.css" rel="stylesheet" type="text/css">
+<title><?= $pageTitle ?></title>

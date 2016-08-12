024d71c09bea1453c62ba11ee8ceae77a9aa600c
Anthony <martab7.am@gmail.com>
2016-08-12 08:41:58 -0500

    Creates php util for uploading files that can save to img folder in public

---
+++ views/partials/uploadForm.php
@@ -0,0 +1,7 @@
+<div class="fileuploadholder">
+  <form enctype="multipart/form-data" action="tester.php" method="post" name="FileUploadForm" id="FileUploadForm">
+    <label for="UploadFileField"></label>
+    <input type="file" name="UploadFileField" id="UploadFileField">
+    <input type="submit" name="UploadButton" id="UploadButton" value="Upload">
+  </form>
+</div>
